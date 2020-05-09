39f61c507c4f94c7be7de4e3a9484fdc8aeb07ad
rob jahn <robjahn@gmail.com>
2020-05-08 22:06:52 -0400

    add echo statements

--- installEZtravel.sh
+++ installEZtravel.sh
@@ -6,15 +6,17 @@
 # - EasyTravel, Legacy 8080,8079 / Angular 9080 and 80 / WebLauncher 8094 / EasyTravel REST 8091 1697
 # - nginx proxy Docker image that is setup & run to redirect to expose and map port 80 to 9080 
 
+echo "*** Starting EZ Travel Install Done."
+
 LOGFILE='/tmp/installEZtravel.log' 
 UNIX_USER_HOME_PATH=/home/root
 mkdir -p $UNIX_USER_HOME_PATH
 
-##Create installer Logfile
+echo "*** Create EZ Travel Install Logfile: $LOGFILE ***"
 printf "\n\n***** Init Installation ***\n" >> $LOGFILE 2>&1 
 { date ; apt update; whoami ; } >> $LOGFILE ; chmod 777 $LOGFILE
 
-# Add ProTip alias
+echo "*** Add ProTip alias ***"
 printf "\n\n***** ProTip Alias***\n" >> $LOGFILE 2>&1 
 echo "
 # Alias for ease of use of the CLI
@@ -24,14 +26,15 @@ alias vaml='vi -c \"set syntax:yaml\" -'
 alias vson='vi -c \"set syntax:json\" -' 
 alias pg='ps -aux | grep' " > /root/.bash_aliases
 
-# Copy Aliases
+echo "*** Copy Aliases ***"
 cp /root/.bash_aliases $UNIX_USER_HOME_PATH/.bash_aliases
 
-## Installation of Chromium on the system
+echo "*** Installation of Chromium on the system ***"
 printf "\n\n***** Installation of Chromium on the system ***\n" >> $LOGFILE 2>&1 
 apt install chromium-browser -y >>  $LOGFILE 2>&1
 
-# NGINX ReverseProxy for AngularShop mapping 9080 to 80 avoid problems on student browsers that 
+echo "*** Add NGINX ReverseProxy for AngularShop ***"
+# mapping 9080 to 80 avoid problems on student browsers that 
 # can hit non-standard ports
 printf "\n\n***** Configuring reverse proxy***\n" >> $LOGFILE 2>&1 
 export PUBLIC_IP=`hostname -i | awk '{ print $1'}`
@@ -47,19 +50,18 @@ server {
     }
 }" > $UNIX_USER_HOME_PATH/nginx/angular.conf
 
-# Install  default-jre
+echo "*** Install  default-jre ***"
 printf "\n\n***** JavaRuntime  install ***\n" >> $LOGFILE 2>&1 
 apt install -y openjdk-8-jre-headless >> $LOGFILE 2>&1
 
-# EasyTravel Angular
+echo "*** Download EasyTravel Angular ***"
 printf "\n\n***** Download, install and configure EasyTravel***\n" >> $LOGFILE 2>&1 
-# Install Easytravel with Angular shop
 { cd $UNIX_USER_HOME_PATH ;\
  wget -nv -O dynatrace-easytravel-linux-x86_64.jar http://dexya6d9gs5s.cloudfront.net/latest/dynatrace-easytravel-linux-x86_64.jar ;\
  java -jar dynatrace-easytravel-linux-x86_64.jar -y ;\
  chmod 755 -R easytravel-2.0.0-x64 ; }  >> $LOGFILE 2>&1  
 
-# Configuring EasyTravel Memory Settings, Angular Shop and Weblauncher. 
+echo "*** Configuring EasyTravel Settings ***"
 sed -i 's/apmServerDefault=Classic/apmServerDefault=APM/g' $UNIX_USER_HOME_PATH/easytravel-2.0.0-x64/resources/easyTravelConfig.properties
 sed -i 's/config.frontendJavaopts=-Xmx160m/config.frontendJavaopts=-Xmx320m/g' $UNIX_USER_HOME_PATH/easytravel-2.0.0-x64/resources/easyTravelConfig.properties
 sed -i 's/config.backendJavaopts=-Xmx64m/config.backendJavaopts=-Xmx320m/g' $UNIX_USER_HOME_PATH/easytravel-2.0.0-x64/resources/easyTravelConfig.properties
@@ -78,7 +80,9 @@ sed -i 's/config.maximumChromeDriversMobile=10/config.maximumChromeDriversMobile
 sed -i 's/config.reUseChromeDriverFrequency=4/config.reUseChromeDriverFrequency=3/g' $UNIX_USER_HOME_PATH/easytravel-2.0.0-x64/resources/easyTravelConfig.properties
 #sed -i 's/config.angularFrontendPortRangeStart=9080/config.angularFrontendPortRangeStart=80/g' /easytravel-2.0.0-x64/resources/easyTravelConfig.properties
 
-# Fix finding the Java package
+echo "*** Fix finding the Java package path ***"
 sed -i "s/JAVA_BIN=..\\/jre\\/bin\\/java/JAVA_BIN=\\/usr\\/bin\\/java/g" $UNIX_USER_HOME_PATH/easytravel-2.0.0-x64/weblauncher/weblauncher.sh
 
 { date ; echo "installation done" ;} >> $LOGFILE 2>&1
+
+echo "*** EZ Travel Install Done."
