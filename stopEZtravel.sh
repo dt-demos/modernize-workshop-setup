61c4cfc750333cc1d9bed6a9e6367ad7655c7ddf
rob jahn <robjahn@gmail.com>
2020-05-11 13:24:56 -0400

    add stop java

--- stopEZtravel.sh
+++ stopEZtravel.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-echo "***Stopping EasyTravel**"
+echo "*** Stopping EasyTravel ***"
 
 if [ "$(ps aux | grep -i /home/workshop/easytravel | wc -l)" != "1" ]; then
   echo stopping easytravel processes
   kill `ps aux | grep -i /home/workshop/easytravel | awk '{print $2}'`
 else
   echo "No easytravel processes to stop"
+fi
+
+if [ "$(ps aux | grep -i com.dynatrace.easytravel | wc -l)" != "1" ]; then
+  echo stopping easytravel Java processes
+  kill `ps aux | grep -i com.dynatrace.easytravel | awk '{print $2}'`
+else
+  echo "No easytravel Java processes to stop"
 fi
 
 CONTAINER=$(sudo docker ps -f name=reverseproxy -q)
