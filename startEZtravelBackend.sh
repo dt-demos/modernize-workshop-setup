07bedfcb73330496fa24c7aaa5f27333ba8fadb2
rob jahn <robjahn@gmail.com>
2020-05-13 15:46:42 -0400

    updates from testing

--- startEZtravelBackend.sh
+++ startEZtravelBackend.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-echo ""
-echo ""
-PRIVATE_IP=`hostname -i | awk '{ print $1'}`
-if [ "$(curl -s http://$PRIVATE_IP:28018/services/ConfigurationService/ping | grep pong | wc -l)" == "1" ]; then
-  echo "Backend is running"
-else
-  echo "Backend not responding"
-fi
-
-exit
 if [ "$(docker ps -a -q)" != "" ]; then
-  echo "*** Stopping and removing all Docker containers ***"
+  echo "*** Stopping and removing EZ Travel Backend and Mongo Docker containers ***"
   docker stop $(docker ps -a -q)
   docker rm $(docker ps -a -q)
 fi
@@ -26,22 +16,23 @@ echo "*** Starting Backend Docker container ***"
 
 PRIVATE_IP=`hostname -i | awk '{ print $1'}`
 
-docker run -p 8091:8091 -d --name backend \
+docker run -p 8091:8080 -d --name backend \
     -e CATALINA_OPTS="-Dconfig.apmServerDefault=${ET_APM_SERVER_DEFAULT} -Xmx300m" \
     -e ET_DATABASE_LOCATION="$PRIVATE_IP:27017" \
     dynatrace/easytravel-backend
 
 echo "*** Running containers ***"
 echo ""
-
 docker ps
 
 echo ""
+echo "*** Sleeping 10 seconds to allow backend to startup"
+sleep 10
 echo ""
-if [ "$(curl -s http://$PRIVATE_IP:28018/services/ConfigurationService/ping | grep pong | wc -l)" == "1" ]; then
+if [ "$(curl -s http://$PRIVATE_IP:8091/services/ConfigurationService/ping | grep pong | wc -l)" == "1" ]; then
   echo "Backend is running"
 else
-  echo "Backend not responding"
+  echo "ERROR: Backend not responding"
 fi
 
 echo "*** Done. ***"
