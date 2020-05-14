4c2a5b3bfb1f5365fb2df04901ac007414b30d16
rob jahn <robjahn@gmail.com>
2020-05-14 16:15:20 -0400

    show one url

--- showEZtravel.sh
+++ showEZtravel.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 echo ""
-echo "easyTravel Angular URL: http://$(curl -s http://checkip.amazonaws.com/)"
-echo "easyTravel Orange URL: http://$(curl -s http://checkip.amazonaws.com/):8080"
+echo "easyTravel URL: http://$(curl -s http://checkip.amazonaws.com/)"
