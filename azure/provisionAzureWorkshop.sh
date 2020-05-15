0a45e4f6641dce42ace0f2c854079a7fa0cd4ffc
rob jahn <robjahn@gmail.com>
2020-05-15 19:22:23 -0400

    dont provision unix and win

--- azure/provisionAzureWorkshop.sh
+++ azure/provisionAzureWorkshop.sh
@@ -8,8 +8,8 @@ echo "=========================================="
 ./loadConfig.sh
 ./createHosts.sh ez
 ./createHosts.sh ez-backend 1 yes
-./createHosts.sh win 3 yes
-./createHosts.sh linux 3 yes
+#./createHosts.sh win 3 yes
+#./createHosts.sh linux 3 yes
 echo ""
 echo "============================================="
 echo "Provision Azure workshop resources COMPLETE"
