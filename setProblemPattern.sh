18db546495d6159c307e45dfe02e99641c4c4f55
rob jahn <robjahn@gmail.com>
2020-05-15 11:57:21 -0400

    add default of true

--- setProblemPattern.sh
+++ setProblemPattern.sh
@@ -3,6 +3,10 @@
 PROBLEM_PATTERN=$1
 ENABLED=$2
 
+if [ -z $ENABLED ]; then
+  ENABLED=true
+fi
+
 if [[ "$ENABLED" != "true" && "$ENABLED" != "false" ]]; then
    echo "ERROR: invalid ENABLED argument. Must pass 'true' or 'false'"
    exit 1
@@ -15,11 +19,11 @@ fi
 
 MY_IP=$(curl -s http://checkip.amazonaws.com/)
 
-echo "*** Setting $PROBLEM_PATTERN on $MY_IP to $ENABLED ***"
+echo "*** Setting $PROBLEM_PATTERN on $MY_IP ***"
 STATUS_CODE=$(curl --write-out %{http_code} --silent --output /dev/null "http://$MY_IP:8091/services/ConfigurationService/setPluginEnabled?name=$PROBLEM_PATTERN&enabled=$ENABLED")
 if [[ "$STATUS_CODE" -ne 202 ]] ; then
   echo "ERROR: Received STATUS_CODE = $STATUS_CODE"
   exit 1
 else
-  echo "Done. Value set."
+  echo "Done. Value set to $ENABLED."
 fi
