3669cf3ae0f22e8f05c05a4ae315c8375edb340a
rob jahn <robjahn@gmail.com>
2020-05-14 22:15:46 -0400

    merge showme into one script

--- showEZtravel.sh
+++ showEZtravel.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 echo ""
-echo "easyTravel URL: http://$(curl -s http://checkip.amazonaws.com/)"
+echo "My hostname    : $(hostname)"
+echo "My public  IP  : $(curl -s http://checkip.amazonaws.com/)"
+echo "My private IP  : $(hostname -i | awk '{ print $1'})"
+echo ""
+echo "easyTravel URL : http://$(curl -s http://checkip.amazonaws.com/)"
--- showMe.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo ""
-echo "My hostname       : $(hostname)"
-echo "My public  IP     : $(curl -s http://checkip.amazonaws.com/)"
-echo "My private IP     : $(hostname -i | awk '{ print $1'})"
