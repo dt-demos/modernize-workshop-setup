8ef162f061090847c20ee13782f03fa1cb46bdd3
rob jahn <robjahn@gmail.com>
2020-05-26 17:43:34 -0400

    adjust problem pattern names

--- setProblemPattern-CreditCardCheckError500.sh
+++ setProblemPattern-CreditCardCheckError500.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-PROBLEM_PATTERN=CPULoadJourneyService
+PROBLEM_PATTERN=CreditCardCheckError500
 ENABLED=$1
+
 
 ./_setProblemPattern.sh $PROBLEM_PATTERN $ENABLED
 
@@ -9,5 +10,6 @@ echo ""
 echo "--------------------------------------------------------------------------------------"
 echo "$PROBLEM_PATTERN"
 echo "--------------------------------------------------------------------------------------"
-echo "Causes a response time issue impacting multiple services. " 
-echo "Root cause is checkDesination Service in Journey Service from a high CPU function call"
+echo "Causes Error in the Booking Service"
+echo "Root cause is Communication plugin could not contact credit card verification application via named pipe"
+echo "in the Booking Service" 
