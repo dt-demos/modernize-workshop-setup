6d0539fd8e8fa0323c0578896774d7208c5ae002
rob jahn <robjahn@gmail.com>
2020-06-19 11:15:34 -0400

    rename from loadDynatraceConfig.sh to  setupDynatraceConfig.sh

---
+++ dynatrace/setupWorkshopConfig.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+source ./dynatraceConfig.lib
+
+echo ""
+echo "-----------------------------------------------------------------------------------"
+echo "Setting up Dynatrace config for $DT_BASEURL"
+echo "-----------------------------------------------------------------------------------"
+echo ""
+
+addConfig dashboards modernize-workshop ./dynatrace/dashboard-workshop.json
+
+setFrequentIssueDetectionOff
+
+setServiceAnomalyDetection ./dynatrace/service-anomalydetection.json
+
+addConfig "service/customServices/java" CheckDestination ./dynatrace/customService-CheckDestination.json
+
+addConfig managementZones ez-travel ./dynatrace/mz-eztravel.json
+addConfig managementZones ez-travel-docker ./dynatrace/mz-eztravel-docker.json
+
+addConfig autoTags workshop-group ./dynatrace/autoTags-workshop-group.json
+
+addConfig "applications/web" EasyTravelOrange ./dynatrace/app-EasyTravelOrange.json
+addConfig "applications/web" EasyTravelOrangeDocker ./dynatrace/app-EasyTravelOrangeDocker.json
+
+echo ""
+echo "-----------------------------------------------------------------------------------"
+echo "Done Setting up Dynatrace config"
+echo "-----------------------------------------------------------------------------------"
