8b5452a02537bb016bc96380a5836c954c7d1518
rob jahn <robjahn@gmail.com>
2020-06-19 11:11:28 -0400

    add cleanup scripts

---
+++ aws/cleanupAWSWorkshop.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+CREDS_FILE=creds.json
+if ! [ -f "$CREDS_FILE" ]; then
+  echo "ERROR: missing $CREDS_FILE"
+  exit 1
+fi
+
+AWS_PROFILE=$(cat creds.json | jq -r '.AWS_PROFILE')
+AWS_REGION=$(cat creds.json | jq -r '.AWS_REGION')
+AWS_KEYPAIR_NAME=$(cat creds.json | jq -r '.AWS_KEYPAIR_NAME')
+RESOURCE_PREFIX=$(cat creds.json | jq -r '.RESOURCE_PREFIX')
+
+STACK_NAME="$RESOURCE_PREFIX-dynatrace-modernize-workshop"
+
+delete_keypair()
+{
+  KEYPAIR_NAME=$1
+  echo ""
+  echo "-----------------------------------------------------------------------------------"
+  echo "Checking to see if $KEYPAIR_NAME exists"
+  echo "-----------------------------------------------------------------------------------"
+
+  aws ec2 describe-key-pairs \
+    --key-names "$KEYPAIR_NAME" \
+    --profile $AWS_PROFILE \
+    --region $AWS_REGION \
+    --output text --query 'KeyPairs[*].[KeyName]' > /dev/null
+  if [ $? == 0 ] ; then
+    echo "Deleting $KEYPAIR_NAME ($INSTANCE_ID)"
+    aws ec2 delete-key-pair \
+      --key-name $KEYPAIR_NAME \
+      --profile $AWS_PROFILE \
+      --region $AWS_REGION
+  else
+    echo ""
+    echo "Skipping, delete key-pair $KEYPAIR_NAME since it does not exists"
+  fi
+}
+
+delete_stack()
+{
+  echo ""
+  echo "-----------------------------------------------------------------------------------"
+  echo "Reqesting CloudFormation Delete Stack $STACK_NAME"
+  echo "-----------------------------------------------------------------------------------"
+
+  aws cloudformation delete-stack \
+      --stack-name $STACK_NAME \
+      --profile $AWS_PROFILE \
+      --region $AWS_REGION
+
+  echo ""
+  echo "Monitor CloudFormation stack status @ https://console.aws.amazon.com/cloudformation/home"
+  echo ""
+}
+
+#*********************************
+echo "==================================================================================="
+echo "Cleaning Up AWS workshop resources"
+echo "Starting: $(date)"
+echo "==================================================================================="
+
+delete_keypair $AWS_KEYPAIR_NAME
+delete_stack
+
+echo "==================================================================================="
+echo "Cleaning Up AWS workshop resources COMPLETE"
+echo "End: $(date)"
+echo "============================================="
