52b631e690c193a706b81d405256cd0aacb55d00
rob jahn <robjahn@gmail.com>
2020-08-31 16:30:06 -0400

    initial file

---
+++ aws/getProblemPatterns.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+CREDS_FILE=creds.json
+if ! [ -f "$CREDS_FILE" ]; then
+  echo "ERROR: missing $CREDS_FILE"
+  exit 1
+fi
+
+AWS_PROFILE=$(cat $CREDS_FILE | jq -r '.AWS_PROFILE')
+AWS_REGION=$(cat $CREDS_FILE | jq -r '.AWS_REGION')
+RESOURCE_PREFIX=$(cat $CREDS_FILE | jq -r '.RESOURCE_PREFIX')
+HOSTNAME="$RESOURCE_PREFIX-dynatrace-modernize-workshop-ez"
+
+PUBLIC_IP="$(aws ec2 describe-instances \
+  --filters "Name=instance-state-name,Values=running" "Name=tag:Name,Values=$HOSTNAME" \
+  --profile $AWS_PROFILE \
+  --region $AWS_REGION \
+  | jq -r '.Reservations[0].Instances[0].PublicIpAddress' )"
+    
+echo ""
+echo "--------------------------------------------------------------------------------------"
+echo "Enabled Patterns on $HOSTNAME ($PUBLIC_IP)"
+echo "--------------------------------------------------------------------------------------"
+curl -s "http://$PUBLIC_IP:8091/services/ConfigurationService/getEnabledPluginNames" | \
+  sed -e 's|<ns:getEnabledPluginNamesResponse xmlns:ns=\"http://webservice.business.easytravel.dynatrace.com\">||g' | \
+  sed -e 's|</ns:getEnabledPluginNamesResponse>||g' | \
+  sed -e 's|</ns:return><ns:return>|\n|g' | \
+  sed -e 's|<ns:return>|\n|g' | \
+  sed -e 's|</ns:return>|\n|g'
+echo ""
+echo ""
