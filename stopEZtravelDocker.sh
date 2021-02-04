159a01402169f5ab9066cb9957f4310913155f7a
robertjahn <rob.jahn@gmail.com>
2021-02-03 20:03:33 -0500

    initial file

---
+++ stopEZtravelDocker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if ! [ $(id -u) = 0 ]; then
+   echo "ERROR: script must be run as root or with sudo"
+   exit 1
+fi
+
+echo "*** Stopping EasyTravel Docker ***"
+
+sudo docker-compose -f $UNIX_USER_HOME_PATH/modernize-workshop-setup/docker-compose.yaml down
+
+echo "*** Stopping EasyTravel Docker Done. ***"
