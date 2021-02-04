8d465d717ddca39e1a48ffaf061f7ad926261441
robertjahn <rob.jahn@gmail.com>
2021-02-03 20:28:57 -0500

    revert original stop logi

--- stopEZtravelMonolith.sh
+++ stopEZtravelMonolith.sh
@@ -5,8 +5,38 @@ if ! [ $(id -u) = 0 ]; then
    exit 1
 fi
 
-echo "*** Stopping EasyTravel Docker ***"
+echo "*** Stopping EasyTravel ***"
 
-sudo docker-compose down
+if [ "$(ps aux | grep -i /home/workshop/easytravel | wc -l)" != "1" ]; then
+  echo "Stopping easytravel processes"
+  kill `ps aux | grep -i /home/workshop/easytravel | awk '{print $2}'`
+else
+  echo "No easytravel processes to stop"
+fi
 
-echo "*** Stopping EasyTravel Docker Done. ***"
+if [ "$(ps aux | grep -i com.dynatrace.easytravel | wc -l)" != "1" ]; then
+  echo "Stopping easytravel Java processes"
+  kill `ps aux | grep -i com.dynatrace.easytravel | awk '{print $2}'`
+else
+  echo "No easytravel Java processes to stop"
+fi
+
+CONTAINER=$(sudo docker ps -f name=reverseproxy-classic -q)
+if [ "$CONTAINER" != "" ]; then
+  echo "removing reverseproxy-classic Docker container"
+  docker stop $CONTAINER
+  docker rm $CONTAINER
+else
+  echo "No reverseproxy-classic container to stop"
+fi
+
+CONTAINER=$(sudo docker ps -f name=reverseproxy-angular -q)
+if [ "$CONTAINER" != "" ]; then
+  echo "removing reverseproxy-angular Docker container"
+  docker stop $CONTAINER
+  docker rm $CONTAINER
+else
+  echo "No reverseproxy-angular container to stop"
+fi
+
+echo "*** Stopping EasyTravel Done. ***"
